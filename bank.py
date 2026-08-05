1a71e2b859cf63450981ae4b11714aa0759e76f8
Mario Maduka <chikanonsondu@gmail.com>
2026-08-05 09:25:05 +0100

    Original Bank code

--- bank.py
+++ bank.py
@@ -1,51 +1,99 @@
-import numpy as np
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-import seaborn as sns
-import matplotlib.pyplot as plt
-import warnings
-import jupyter as jt
-
-
 user_name = "Ugwu Bernard"
 password = "2011"
 opening_balance = 20000
 
+def bank_action():
 
-
-# while True:
- 
-#  print("Welcome, ", user_name, ". \nBalance: ", opening_balance)
-#  amount_to_withdraw = float(input("Enter the amount you want to withdraw: "))
-#  if amount_to_withdraw > opening_balance:
-#     print("Insufficient fund!⛔")
-#     break
-#  elif amount_to_withdraw <= opening_balance:
-#    print("Successful transaction.")
-#    closing_balance = opening_balance - amount_to_withdraw
-#    opening_balance = closing_balance
-#    print("Your balance is: ",closing_balance)
-#    choice_to_continue = input("do you want to continue(y/n): ")
-#    if choice_to_continue == "y" and closing_balance > 1000:
-#      continue
-#    elif closing_balance <= 1000:
-#      print("Low funds🫙. You've reached your daily limit.")
-#      break
-      
-#    else:
-#       print("Sorry, you can't continue.You have exhausted your balance")
-#       break
- 
-# def simple_interest(p, r, t):
-#   return (p * r * t) / 100
-
-
-# print("The simple instrest of Principal 💲8000, rate 90 and time of 5 years is: ",simple_interest(8000, 9, 5))
- 
+ opening_balance = 20000
+ while True:
    
-def addition(a,b):
-    return a + b
-def subtraction(a,b):
-    return a - b
+  amount_to_withdraw = float(input("Enter the amount you want to withdraw: "))
+  if amount_to_withdraw > opening_balance:
+         print("Insufficient fund! ⛔")
+         break
+  elif amount_to_withdraw <= opening_balance:
+       print("Successful Transaction!")
+       closing_balance = opening_balance - amount_to_withdraw
+       opening_balance = closing_balance
+       print("Your balance is ", opening_balance)
+       choice_to_continue = input("Do you want to proceed(y/n): ")
+       if choice_to_continue == 'y' and opening_balance > 1000:
+         continue
+       elif opening_balance == 1000:
+         print("Low funds 🫙. You've reached your daily limit.")
+         break
+       elif opening_balance == 0:
+         print("Sorry, you can't continue. You've exhausted your balance.")
+         break
+       if choice_to_continue == 'n':
+         break
+
+def deposit_money():
+  print("Welcome, ", user_name)
+  amount_to_deposit = float(input("Enter the amount you want to deposit: "))
+  new_opening_balance_2 = amount_to_deposit + new_opening_balance
+  code_to_check_balance = "*989*7#"
+  print("Succesful Deposition!\nTo check your balance enter: ", code_to_check_balance)
+  code = input("Enter the code: ")
+  if code == code_to_check_balance:
+        print("Your balance is: ", new_opening_balance_2)
+def new_bank_action():
+ opening_balance = 20000
+ opening_balance = amount_to_deposit + opening_balance
+ while True:
+   
+  amount_to_withdraw = float(input("Enter the amount you want to withdraw: "))
+  if amount_to_withdraw > opening_balance:
+         print("Insufficient fund! ⛔")
+         break
+  elif amount_to_withdraw <= opening_balance:
+       print("Successful Transaction!")
+       closing_balance = opening_balance - amount_to_withdraw
+       opening_balance = closing_balance
+       print("Your balance is ", opening_balance)
+       choice_to_continue = input("Do you want to proceed(y/n): ")
+       if choice_to_continue == 'y' and opening_balance > 1000:
+         continue
+       elif opening_balance <= 1000:
+         print("Low funds 🫙. You've reached your daily limit.")
+         break
+       elif opening_balance == 0:
+         print("Sorry, you can't continue. You've exhausted your balance.")
+         break
+       if choice_to_continue == 'n':
+         break
+print("Welcome, ", user_name, ". \nBalance: ", opening_balance)
+choice = float(input("1. Withdraw\n2. Deposit money\n"))
+if choice == 1:
+  password_check = input("Enter your Password:\n")
+  if password_check == password: 
+    bank_action()
+  elif password_check != password:
+      print("❌ Wrong Password!")
+      password_check_again = input("Try again:\n")
+      if password_check_again == password:
+        bank_action()    # break
+
+if choice == 2:
+     print("Welcome, ", user_name)
+     amount_to_deposit = float(input("Enter the amount you want to deposit: "))
+     new_opening_balance = amount_to_deposit + opening_balance
+     code_to_check_balance = "*989*7#"
+     print("Succesful Deposition!\nTo check your balance enter: ", code_to_check_balance)
+     code = input("Enter the code: ")
+     if code == code_to_check_balance:
+        print("Your balance is: ", new_opening_balance)
+        print("Welcome, ", user_name, ". \nBalance: ", new_opening_balance)
+        choice = float(input("1. Withdraw\n2. Deposit money\n"))
+        if choice == 1:
+           new_bank_action()
+        if choice == 2: 
+           deposit_money()
+           
+           
+           
+           
+
+ 
 
 

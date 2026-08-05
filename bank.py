9ed27ce34f87da7306b886e0708c5f82978ec467
Mario Maduka <chikanonsondu@gmail.com>
2026-08-05 10:10:04 +0100

    Upgraded by adding SQLite

--- bank.py
+++ bank.py
@@ -1,7 +1,3 @@
-user_name = "Ugwu Bernard"
-password = "2011"
-opening_balance = 20000
-
 def bank_action():
 
  opening_balance = 20000
@@ -27,16 +23,6 @@ def bank_action():
          break
        if choice_to_continue == 'n':
          break
-
-def deposit_money():
-  print("Welcome, ", user_name)
-  amount_to_deposit = float(input("Enter the amount you want to deposit: "))
-  new_opening_balance_2 = amount_to_deposit + new_opening_balance
-  code_to_check_balance = "*989*7#"
-  print("Succesful Deposition!\nTo check your balance enter: ", code_to_check_balance)
-  code = input("Enter the code: ")
-  if code == code_to_check_balance:
-        print("Your balance is: ", new_opening_balance_2)
 def new_bank_action():
  opening_balance = 20000
  opening_balance = amount_to_deposit + opening_balance
@@ -62,33 +48,90 @@ def new_bank_action():
          break
        if choice_to_continue == 'n':
          break
-print("Welcome, ", user_name, ". \nBalance: ", opening_balance)
-choice = float(input("1. Withdraw\n2. Deposit money\n"))
-if choice == 1:
-  password_check = input("Enter your Password:\n")
-  if password_check == password: 
-    bank_action()
-  elif password_check != password:
-      print("❌ Wrong Password!")
-      password_check_again = input("Try again:\n")
-      if password_check_again == password:
-        bank_action()    # break
+def deposit_money():
+  print("Welcome, ", user_name)
+  amount_to_deposit = float(input("Enter the amount you want to deposit: "))
+  new_opening_balance_2 = amount_to_deposit + new_opening_balance
+  code_to_check_balance = "*989*7#"
+  print("Succesful Deposition!\nTo check your balance enter: ", code_to_check_balance)
+  code = input("Enter the code: ")
+  if code == code_to_check_balance:
+        print("Your balance is: ", new_opening_balance_2)
 
-if choice == 2:
-     print("Welcome, ", user_name)
-     amount_to_deposit = float(input("Enter the amount you want to deposit: "))
-     new_opening_balance = amount_to_deposit + opening_balance
-     code_to_check_balance = "*989*7#"
-     print("Succesful Deposition!\nTo check your balance enter: ", code_to_check_balance)
-     code = input("Enter the code: ")
-     if code == code_to_check_balance:
-        print("Your balance is: ", new_opening_balance)
-        print("Welcome, ", user_name, ". \nBalance: ", new_opening_balance)
-        choice = float(input("1. Withdraw\n2. Deposit money\n"))
-        if choice == 1:
-           new_bank_action()
-        if choice == 2: 
-           deposit_money()
+       
+import sqlite3 as sql
+
+connection = sql.connect("database.db")
+
+cursor = connection.cursor()
+password = "2011"
+opening_balance = 20000
+username = str(input("Username: "))
+password = str(input("Password: "))
+user_name = username
+
+cursor.execute("""
+SELECT *
+FROM Users
+WHERE Username = ?
+AND Password = ?
+""", (username, password))
+
+
+user = cursor.fetchone()
+
+if user:
+    print("Welcome, ", user_name, ". \nBalance: ", opening_balance)
+    choice = float(input("1. Withdraw\n2. Deposit money\n"))
+    if choice == 1:
+       bank_action()
+    if choice == 2:
+       print("Welcome, ", user_name)
+       amount_to_deposit = float(input("Enter the amount you want to deposit: "))
+       new_opening_balance = amount_to_deposit + opening_balance
+       code_to_check_balance = "*989*7#"
+       print("Succesful Deposition!\nTo check your balance enter: ", code_to_check_balance)
+       code = input("Enter the code: ")
+       if code == code_to_check_balance:
+          print("Your balance is: ", new_opening_balance)
+          print("Welcome, ", user_name, ". \nBalance: ", new_opening_balance)
+          choice = float(input("1. Withdraw\n2. Deposit money\n"))
+          if choice == 1:
+             new_bank_action()
+          if choice == 2:
+             deposit_money()
+       
+
+else:
+   print("Error")
+
+# print("Welcome, ", user_name, ". \nBalance: ", opening_balance)
+# choice = float(input("1. Withdraw\n2. Deposit money\n"))
+# if choice == 1:
+#   password_check = input("Enter your Password:\n")
+#   if password_check == password: 
+#     bank_action()
+#   elif password_check != password:
+#       print("❌ Wrong Password!")
+#       password_check_again = input("Try again:\n")
+#       if password_check_again == password:
+#         bank_action()    # break
+
+# if choice == 2:
+#      print("Welcome, ", user_name)
+#      amount_to_deposit = float(input("Enter the amount you want to deposit: "))
+#      new_opening_balance = amount_to_deposit + opening_balance
+#      code_to_check_balance = "*989*7#"
+#      print("Succesful Deposition!\nTo check your balance enter: ", code_to_check_balance)
+#      code = input("Enter the code: ")
+#      if code == code_to_check_balance:
+#         print("Your balance is: ", new_opening_balance)
+#         print("Welcome, ", user_name, ". \nBalance: ", new_opening_balance)
+#         choice = float(input("1. Withdraw\n2. Deposit money\n"))
+#         if choice == 1:
+#            new_bank_action()
+#         if choice == 2: 
+#            deposit_money()
            
            
            
